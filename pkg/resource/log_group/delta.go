792614a414a9067d9382413123cc2cfb4acf7ba1
ci-robot <ci-robot@k8s.io>
2025-03-27 16:17:52 +0000

    Update to ACK runtime `v0.44.0`, code-generator `v0.44.0`

--- pkg/resource/log_group/delta.go
+++ pkg/resource/log_group/delta.go
@@ -75,7 +75,9 @@ func newResourceDelta(
 			delta.Add("Spec.SubscriptionFilters", a.ko.Spec.SubscriptionFilters, b.ko.Spec.SubscriptionFilters)
 		}
 	}
-	if !ackcompare.MapStringStringEqual(ToACKTags(a.ko.Spec.Tags), ToACKTags(b.ko.Spec.Tags)) {
+	desiredACKTags, _ := convertToOrderedACKTags(a.ko.Spec.Tags)
+	latestACKTags, _ := convertToOrderedACKTags(b.ko.Spec.Tags)
+	if !ackcompare.MapStringStringEqual(desiredACKTags, latestACKTags) {
 		delta.Add("Spec.Tags", a.ko.Spec.Tags, b.ko.Spec.Tags)
 	}
 
